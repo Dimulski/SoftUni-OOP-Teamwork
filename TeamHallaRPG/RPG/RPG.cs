efffb852f128151a7c0203f13c745800b31218d0
Dimulski <george.dimulski@gmail.com>
2015-12-10 20:33:02 +0200

    Added some labels and a test button

---
+++ TeamHallaRPG/RPG/RPG.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG
+{
+    public partial class RPG : Form
+    {
+        public RPG()
+        {
+            InitializeComponent();
+        }
+
+        private void RPG_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+    }
+}
